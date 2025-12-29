0d1c0ea3e98d0e29d533e0d9804ff7273dc52f89
Nakta-Veda <moneydon77@naver.com>
2025-12-29 11:58:43 +0900

    데몬 잠시 끄고 테스트

--- main.c
+++ main.c
@@ -10,16 +10,16 @@ void sig_handler(int signo) {
 }
 
 int main(int argc, char** argv) {
-    if (daemon(1, 0) == -1) {
-        perror("daemon");
-        exit(1);
-    }
+    // if (daemon(1, 0) == -1) {
+    //     perror("daemon");
+    //     exit(1);
+    // }
 
-    struct sigaction act;
-    act.sa_handler = sig_handler;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = SA_RESTART;
-    sigaction(SIGTERM, &act, NULL); // kill 신호
+    // struct sigaction act;
+    // act.sa_handler = sig_handler;
+    // sigemptyset(&act.sa_mask);
+    // act.sa_flags = SA_RESTART;
+    // sigaction(SIGTERM, &act, NULL); // kill 신호
 
     init_modules(); // 모듈들 init 설정
     run_server();   // 서버 동작
